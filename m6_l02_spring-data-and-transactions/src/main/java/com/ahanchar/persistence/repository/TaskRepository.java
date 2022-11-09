0ae12dfa8b2770305825ae1345295e1f5055c8dd
Aliaksandr <91119461+Sandris-H@users.noreply.github.com>
2022-11-09 19:40:47 +0100

    Add files via upload

---
+++ m6_l02_spring-data-and-transactions/src/main/java/com/ahanchar/persistence/repository/TaskRepository.java
@@ -0,0 +1,9 @@
+package com.ahanchar.persistence.repository;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.ahanchar.persistence.model.Task;
+
+public interface TaskRepository extends CrudRepository<Task, Long> {
+
+}
